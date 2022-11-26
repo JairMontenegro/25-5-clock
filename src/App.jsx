15cd87a9d1e558049edb0eceef4fb273db08beaa
Jair montenegro <jairmontenegrof@gmail.com>
2022-11-26 14:03:52 -0300

    formatter time

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,20 @@ import Timer from "./components/timer";
 import "./App.scss";
 
 function App() {
-  const [breakLength, setBreakLenght] = useState(5);
+  const [breakLength, setBreakLength] = useState(5);
   const [session, setSession] = useState(25);
+  const [play, setPlay] = useState(false);
+  const [type, setType] = useState("Session");
+  const [timeLeft, setTimeLeft] = useState(1500);
 
-  const timerLeft = () => {
-    console.log("hello world");
+  const timer = () => {
+    const conversionMinutes = Math.floor(timeLeft / 60);
+    const conversionSeconds = timeLeft - conversionMinutes * 60;
+    const formattedSeconds =
+      conversionSeconds < 10 ? "0" + conversionSeconds : conversionSeconds;
+    const formattedMinutes =
+      conversionMinutes < 10 ? "0" + conversionMinutes : conversionMinutes;
+    return `${formattedMinutes}:${formattedSeconds}`;
   };
 
   const handleStartStop = () => {
@@ -17,6 +26,8 @@ function App() {
   const handleReset = () => {
     console.log("hello reset ");
   };
+
+  const status = type === "Session" ? "Session" : "Break";
 
   return (
     <div className="App">
@@ -39,14 +50,19 @@ function App() {
           length={session}
         />
 
-        <div id="timer-label">SESSION</div>
-        <div id="timer-left">{timerLeft()}</div>
+        <div id="timer-label">{type}</div>
+        <div id="timer-left">{timer()}</div>
         <button id="start_stop" onClick={handleStartStop}>
           Start/Stop
         </button>
         <button id="reset" onClick={handleReset}>
           Reset
         </button>
+        <audio
+          id="beep"
+          preload="auto"
+          src="https://raw.githubusercontent.com/freeCodeCamp/cdn/master/build/testable-projects-fcc/audio/BeepSound.wav"
+        />
       </div>
     </div>
   );
